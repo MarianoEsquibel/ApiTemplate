33428e48a43f0734988188aaded27700a5f53436
mariano <omarianou@gmail.com>
2025-06-06 22:26:07 -0300

    PRIMER COMMIT

---
+++ WebApplication1/Domain/Others/Constantes/DomainConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Others.Constantes
+{
+    /// <summary>
+    /// CLASE QUE ALMACENA LAS CONSTANTES UTILIZADAS EN LA CAPA DE DOMINIO.
+    /// </summary>
+    internal static class DomainConstant
+    {
+        public const string DUMMY_CONSTANT = "DUMMY_CONSTANT";
+    }
+}
---
+++ WebApplication1/Domain/Others/Utils/DummyUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Others.Utils
+{
+    /// <summary>
+    /// CLASES CON CALCULOS O LOGICA AUXILIAR QUE NO TIENEN QUE VER CON EL DOMINIO DEL PROBLEMA, PUEDE CPNTENER FUNCIONES DE TODO TIPO
+    /// </summary>
+    public static class DummyUtils
+    {
+        /// <summary>
+        /// TODOS LOS METODOS DEBEN SER ESTATICOS.
+        /// </summary>
+        /// METODO DE LA CLASE:
+        public static void DummyMethod()
+        {
+        }
+        /// <summary>
+        /// METODO DE EXTENSIÓN.
+        /// </summary>
+        public static void DummyExtensionMethod(this string args)
+        {
+        }
+    }
+}
